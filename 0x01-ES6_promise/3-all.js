cccf699c1edcbb4f1250abab66288bbfcbd0348d
Esseh12 <gbenmoese788@gmail.com>
2024-07-25 01:40:57 +0100

    multiple promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,12 +1,10 @@
 import { createUser, uploadPhoto } from './utils';
 
 export default function handleProfileSignup() {
-  const promise1 = [createUser()];
-  const promise2 = [uploadPhoto()];
-  Promise.all(promise1, promise2)
+  Promise.all([uploadPhoto(), createUser()])
     .then((results) => {
-      const [ { firstName, lastName }] = results;
-      console.log(` ${firstName} ${lastName}`);
+      const [{ body }, { firstName, lastName }] = results;
+      console.log(`${body} ${firstName} ${lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
