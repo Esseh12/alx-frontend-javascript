6f681b5b17400fd2a4aeda8d49a4a0394726d7bb
Esseh12 <gbenmoese788@gmail.com>
2024-07-27 00:20:07 +0100

    promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,12 +1,11 @@
 import { createUser, uploadPhoto } from './utils';
 
 export default function handleProfileSignup() {
-  try {
-    Promise.all([uploadPhoto(), createUser()])
-      .then(([{ body }, { firstName, lastName }]) => {
-        console.log(`${body} ${firstName} ${lastName}`);
-      });
-  } catch (error) {
-    return console.log('Signup system offline');
-  }
+  Promise.all([uploadPhoto(), createUser()])
+    .then(([photo, user]) => {
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
 }
