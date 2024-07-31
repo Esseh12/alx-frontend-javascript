c843b18c75782ff68ad380bf5a9e163cba952e82
Esseh12 <gbenmoese788@gmail.com>
2024-07-31 01:44:02 +0100

    startString

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,6 @@
 export default function cleanSet(set, startString) {
+  if (!startString || startString.length === 0) return '';
+
   return Array.from(set)
     .filter((value) => value.startsWith(startString))
     .map((value) => value.slice(startString.length))
